b6605f3acd60dcd586c894cd9e2ff2fcab5f3e06
Virmil <vtalattad@egcextremeunrealtechnology.com>
2023-01-25 14:59:30 +0800

    story spacing adjustments

--- resources/views/doc_story_c1.blade.php
+++ resources/views/doc_story_c1.blade.php
@@ -34,39 +34,55 @@ WALK Online - Mobile MMORPG
 <div class="separator-blue-center mt-5 mb-5"></div>
 
 	<pre class="text-light readable">
-In the year 2200, Nations from different countries united with the same goal of human development and, little by little, flourished entirely as a United Global Government. A utopia, before only dreamed of, is now in the grasp of everyone else. Answered problems caused more significant unreasonable complications to arise. With the growing human population, the earth's way to its limit. The demand for food products arises; on the other hand, the W.S.O. (World Sustenance Organization) has predicted the food supply to vanish in the next five years. Technological advancement made its way to fix its production shortage through rapid plant reproduction called extensive apomixis. A process of a plant bypasses the most fundamental aspects of sexual reproduction on a rapid scale, making plants create seeds without fertilization. The W.S.O. (World Sustenance Organization) started a petition to save humanity's extinction signed by the UGG Leaders and proceeded to the project. After a month of successful run-through, the (WorPoLT) World Power, Logistics, and Technology submitted reports to the committee.
+	In the year 2200, Nations from different countries united with the same goal of human development and, little by little, flourished entirely as a United Global Government. A utopia, before only dreamed of, is now in the grasp of everyone else. Answered problems caused more significant unreasonable complications to arise. With the growing human population, the earth's way to its limit. The demand for food products arises; on the other hand, the W.S.O. (World Sustenance Organization) has predicted the food supply to vanish in the next five years. Technological advancement made its way to fix its production shortage through rapid plant reproduction called extensive apomixis. A process of a plant bypasses the most fundamental aspects of sexual reproduction on a rapid scale, making plants create seeds without fertilization. The W.S.O. (World Sustenance Organization) started a petition to save humanity's extinction signed by the UGG Leaders and proceeded to the project. After a month of successful run-through, the (WorPoLT) World Power, Logistics, and Technology submitted reports to the committee.
 
-Dear UGG Leaders
+Dear UGG Leaders,
 The Extensive Apomixes was a success in rapid plant reproduction; however, plant incubators consume power to such a great extent that it requires a massive amount of reactors for it to do. Logistics in our department made our estimations with our current world energy reactors. 
 We are on the brink of a global energy crisis.
  - Arnold Green (WorPolt President)
 
 The confidential message between the world committees got leaked to the public. The Issue of food scarcity, after five years, and the overuse of power plants and reactors that posed a threat to health problems caused doubts in the populace of the (UGG) United Global Government. Dormant enemies of the state began to rise; disarray in the outlying regions started to collapse. The United Global Government is ready for its council to solve yet another problem. All were silent. A mysterious voice raised its way to the ears of the committee.
-"World is at its peak, Mr. Presidents. Our resources cannot reproduce, the power generators pollute our lands, the people are in disarray, and We are facing a mass extinction!"  An unfamiliar voice said.
+
+"World is at its peak, Mr. Presidents. Our resources cannot reproduce, the power generators pollute our lands, the people are in disarray, and we face mass extinction!"  An unfamiliar voice said.
 The council looked down in despair. The mysterious figure unfolded its face; it was General Lakaw; he strolled to the center of the committee. 
-“It was General Lakaw of KAF (Kalaw Armed Forces); the KAF got decommissioned after the UGG successfully united the entire world through their 4Ps (Persuasion, Proposal, and Peace Project) last year. Peace fractured war, and the military lost its purpose. General Lakaw vanished with its élite forces and turned underground. " Head Councilor Karlos Calderon stated.
- "But here I am in front of you! I, General Lakaw, will help you Unite the world again. "General Lakaw exclaimed. 
+
+“It was General Lakaw of KAF (Kalaw Armed Forces); the KAF got decommissioned after the UGG successfully united the entire world through their 4Ps (Persuasion, Proposal, and Peace Project) last year. Peace fractured war and the military lost its purpose. General Lakaw vanished with its élite forces and turned underground. " Head Councilor Karlos Calderon stated.
+ 
+"But here I am in front of you! I, General Lakaw, will help you Unite the world again. "General Lakaw exclaimed. 
 KAF troops marched inside the Hall and rallied themselves along with the UGG. General Lakaw continued, "This is my proposal, gentlemen!" 
 One KAF member threw a projector device in the middle of the council's table, projecting a floating serpent circling a tower. 
+
 "I call this the Bakunawa, not the myth people called it before. This. I say it is a biomechanical product mixed with military technology." General Lakaw looked at the council faces, amused.
- "The Bakunawa is the power stabilizer that prevents the particle generator from over-accelerating, providing us unlimited energy resources." the General continued until,
-"Particle generator, you say? You know how much danger it could lead us to. It could open a gateway to hell if it pushed right through your selfish imaginations!" Karlos Calderon exclaimed. 
+ 
+"The Bakunawa is the power stabilizer that prevents the particle generator from over-accelerating, providing us unlimited energy resources." the General continued until,
+
+ "Particle generator, you say? You know how much danger it could lead us to. It could open a gateway to hell if it pushed right through your selfish imaginations!" Karlos Calderon exclaimed. 
 The general smirked, "And what do you suggest, Mister Kalderon? Let the people, your loved ones, die beside you after five years?” asked the General.
- “As I can recall, you do not represent this council,” the General exclaimed
+ 
+“As I can recall, you do not represent this council,” the General exclaimed
+
 “I know you need me... you need my operation to save this rotten world already crumbling." The General continued.
  The council members mumbled. Without much thought, the council signed General Kalaw's proposal, giving him control over the UGG.
 Three months passed, and the UGG-KAF was able to build the BK-NWA particle accelerator as the UGG-KAF organized a convention, a grand opening of the largest power plant. People around the globe opened their monitors, radio, and media devices; some gathered near the site to see the magnificent view of the biomechanical Bakunawa. Next to the power plant are the leaders of each organization. 
-"We are gathered here to witness the next stage of human development! I, General Lakaw, will take part in this historical event. The ‘children of the future will look upon the stone with our names engraved in it, marking us the forefathers of their Utopian paradise!" General Lakaw exclaimed.
+
+"We are gathered here to witness the next stage of human development! I, General Lakaw, will take part in this historical event. The future children will look upon the stone with our names engraved, marking us the forefathers of their Utopian paradise!" General Lakaw exclaimed.
 The Leaders of the organizations wore their protective goggles. General Lakaw nodded to the WorPolt President, Arnold Green, as he handled the controls to start up the BK-NWA 2200 Particle Generator. 
+
 "One small step, General." Arnold Green whispered while powering the systems of the power plant. 
 The particle generator shined its way above the punctured clouds; the satellite cameras saw the gigantic light emanating from the earth's atmosphere. People around the globe cheered upon the successful execution of the power generator. The General smiled proudly, looking at the organization leaders as they shook each other’s hands. 
 In the middle of the celebration, strange rumblings gathered people’s attention at the convention hall. General Lakaw thought of it as a terrorist attack and contacted the KAF base immediately. The satellite cameras captured fractures on the earth’s crust near their location. Strange creatures emerged from the crevices and started attacking and killing people at the convention; General Lakaw Rallied his troops to protect the leaders. Creatures overwhelmed the General's troops, decimating half of them, and together with the remaining troops, they ran towards the power plant's control room. Creatures banged the reinforced doors of the control room. 
+
 "Arnold, take us out of here!" the General shouted.
+
  "But how?" Arnold Green asked with fear on his face. 
+
 "The power link of the Particle Generator connected directly to the KAF secret base; we can use the Bakunawa's momentum to link us to the exact location," General Lakaw exclaimed.
- "Using the bakunawa? It could malfunction the whole generator and transfer us elsewhere!" Arnold hesitantly replied. 
+ 
+"Using the bakunawa? It could malfunction the whole generator and transfer us elsewhere!" Arnold hesitantly replied. 
+
 "Anywhere but not here." General Lakaw pointed a gun and shot Arnold in cold blood.
- General Lakaw tried to override the power generator.
+ 
+General Lakaw tried to override the power generator.
  Biomechanical Bakunawa circulated rapidly on the power generator, producing massive electrical thunderstorms above the site. The earth rumbled at a 4.0 magnitude until the BK-NWA 2200 Particle Generator suddenly vanished into thin air.
 
 	</pre>
